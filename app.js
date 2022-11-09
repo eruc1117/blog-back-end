db77e6057076977d8f15148d4e5d990e68da8dd5
eruc <eruc101010@gmail.com>
2022-11-09 22:08:42 +0800

    feat: add cors error. Open all origin

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const routes = require('./routes/index')
+const cors = require("cors")
 const app = express()
 
 const PORT = 4000
 
+app.use(cors())
 app.use(routes)
 
 app.listen(PORT, () => {
